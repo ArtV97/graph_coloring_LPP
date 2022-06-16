67d2258464f9b3ff0cbe2e3dd20e87cf3fe4cc37
Arthur <arthurvianna@id.uff.br>
2022-06-16 18:27:19 -0300

    openMP version

---
+++ openMP/util.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BUFFER_SZ 1024
+
+int *read_file(char *filename, int *n);
+
+int **get_uncolored_neighbours(int *graph, int n, int v, int *color);
+
+int **get_neighbours_color(int *graph, int n, int v, int *color);
